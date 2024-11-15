498cef8bd51d5fefdddb47251ed417807a4627bf
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-11-15 22:08:09 +0200

    different movement

--- Scripts/component/movement/pathfind_component.gd
+++ Scripts/component/movement/pathfind_component.gd
@@ -1,8 +1,7 @@
-class_name PathfindComponent extends Node2D
+class_name PathfindComponent extends Area2D
 
 @export var velocity_component: VelocityComponent;
-@export var grid_object_component: GridObjectComponent;
-@export var detection_distance: float = 60;
+@export var self_area: Area2D;
 @export var separation_distance: float = 40;
 @export var separation_weight: float = 0.6;
 @export var target_weight: float = 0.5;
@@ -11,42 +10,42 @@ var target: Node2D;
 
 
 func _process(_delta):
-	if (grid_object_component): velocity_component.set_target(_calculateFlockDirection());
-	else: velocity_component.set_target(_get_target_velocity());
+	velocity_component.set_target(_calculateFlockDirection());
 
 func _calculateFlockDirection():
-	var thisGlobalPosition = global_position;
+	var thisGlobalPosition = get_global_position();
 		
 	var separation = Vector2.ZERO;
 	var targetVec = Vector2.ZERO;
 	
-	var flockmates: Array = GridManager.getObjectsInRadiusAroundObject(grid_object_component, detection_distance);
-	
-	for flockmate in flockmates:
-		if (flockmate == grid_object_component || flockmate == null || flockmate.is_queued_for_deletion()): continue;
+	var flockmates: Array = get_overlapping_areas();
+	for flockmate: Area2D in flockmates:
+		if (flockmate == self_area || flockmate == null || flockmate.is_queued_for_deletion()): continue;
 		
-		var flockmateGlobalPosition = flockmate.global_position;
+		var flockmateGlobalPosition: Vector2 = flockmate.get_global_position();
 		
-		var distanceToFlockmate = thisGlobalPosition.distance_to(flockmateGlobalPosition);
+		var distanceToFlockmate: float = thisGlobalPosition.distance_to(flockmateGlobalPosition);
 		
-		if distanceToFlockmate > detection_distance: continue;
-		
-		separation -= ((flockmateGlobalPosition - thisGlobalPosition).normalized() * velocity_component.max_speed * separation_distance) / distanceToFlockmate
+		#separation += (thisGlobalPosition - flockmateGlobalPosition).normalized() * velocity_component.max_speed;
+		#separation += (thisGlobalPosition - flockmateGlobalPosition).normalized() * velocity_component.max_speed * (distanceToFlockmate / separation_distance);
+		separation -= (flockmateGlobalPosition - thisGlobalPosition).normalized() * (velocity_component.max_speed * (separation_distance / distanceToFlockmate))
+		#separation -= (thisGlobalPosition - flockmateGlobalPosition).normalized() * velocity_component.max_speed * (separation_distance / distanceToFlockmate);
+		#separation -= (flockmateGlobalPosition - thisGlobalPosition).normalized() * velocity_component.max_speed * (distanceToFlockmate / separation_distance);
+		#separation -= (flockmateGlobalPosition - thisGlobalPosition).normalized() * velocity_component.max_speed * (separation_distance / distanceToFlockmate);
 		#separation -= (flockmateGlobalPosition - thisGlobalPosition).normalized() * lerpf(0, velocity_component.max_speed, (separation_distance / distanceToFlockmate));
 	
 	targetVec = _get_target_velocity();
-		
 	var desiredDirection = (
 		separation * separation_weight +
 		targetVec * target_weight
 	);
 	
-	return desiredDirection;
+	return desiredDirection.normalized() * velocity_component.max_speed;
 	
 func _get_target_velocity() -> Vector2:
 	if (!target):
 		push_error("No target!");
 		return Vector2.ZERO;
-	var desiredDirection = (target.global_position - global_position);
+	var desiredDirection: Vector2 = (target.global_position - global_position).normalized();
 		
 	return desiredDirection * velocity_component.max_speed;
--- Scripts/component/movement/ranged_pathfind_component.gd
+++ Scripts/component/movement/ranged_pathfind_component.gd
@@ -1,7 +1,6 @@
 class_name RangedPathfindComponent extends Node2D
 
 @export var velocity_component: VelocityComponent;
-@export var grid_object_component: GridObjectComponent;
 @export var detection_distance: float = 60;
 @export var separation_distance: float = 40;
 @export var separation_weight: float = 0.6;
@@ -12,29 +11,12 @@ var target: Node2D;
 
 
 func _process(_delta):
-	if (grid_object_component): velocity_component.set_target(_calculateFlockDirection());
-	else: velocity_component.set_target(_get_target_velocity());
+	velocity_component.set_target(_get_target_velocity());
 	if (target): rotation = (target.global_position - global_position).normalized().angle();
 
 func _calculateFlockDirection():
-	var thisGlobalPosition = global_position;
-		
 	var separation = Vector2.ZERO;
 	var targetVec = Vector2.ZERO;
-	
-	var flockmates: Array = GridManager.getObjectsInRadiusAroundObject(grid_object_component, detection_distance);
-	
-	for flockmate in flockmates:
-		if (flockmate == grid_object_component || flockmate == null || flockmate.is_queued_for_deletion()): continue;
-		
-		var flockmateGlobalPosition = flockmate.global_position;
-		
-		var distanceToFlockmate = thisGlobalPosition.distance_to(flockmateGlobalPosition);
-		
-		if distanceToFlockmate > detection_distance: continue;
-		
-		separation -= ((flockmateGlobalPosition - thisGlobalPosition).normalized() * velocity_component.max_speed * separation_distance) / distanceToFlockmate
-		#separation -= (flockmateGlobalPosition - thisGlobalPosition).normalized() * lerpf(0, velocity_component.max_speed, (separation_distance / distanceToFlockmate));
 	
 	targetVec = _get_target_velocity();
 		
@@ -49,10 +31,10 @@ func _get_target_velocity() -> Vector2:
 	if (!target):
 		push_error("No target!");
 		return Vector2.ZERO;
-	var desiredDirection = (target.global_position - global_position);
-	var distance_to_target = desiredDirection.length();
+	var desiredDirection: Vector2 = (target.global_position - global_position);
+	var distance_to_target: float = desiredDirection.length();
 	
-	var speed_factor = 0;
+	var speed_factor: float = 0;
 	if distance_to_target < target_separation_distance: speed_factor = 0.01;
 	else: speed_factor = clamp((distance_to_target - separation_distance) / (detection_distance - separation_distance), 0, 1);
 	
